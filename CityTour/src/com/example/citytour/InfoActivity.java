8fa3c8dc3318c582207a0c2d4b8d78640c6b1b50
alvarodelaserna <alvaro.delaserna@gmail.com>
2014-03-11 10:59:22 +0100

    info a tope

--- CityTour/src/com/example/citytour/InfoActivity.java
+++ CityTour/src/com/example/citytour/InfoActivity.java
@@ -111,16 +111,65 @@ public class InfoActivity extends Activity {
 						TEXT = paragraphs[1]+paragraphs[2]+paragraphs[3]+paragraphs[4]+paragraphs[5];
 						correctText = TEXT.replace("<\\" , "<");
 					}else if(url.contains("Museo_del_Pra")){
+						// caso del museo del prado
 						TEXT = paragraphs[1]+paragraphs[2]+paragraphs[3]+paragraphs[4]+paragraphs[5]+paragraphs[6]+paragraphs[7]+paragraphs[8];
 						correctText = TEXT.replace("<\\" , "<");
 					}else if(url.contains("Museo_Thy")){
+						// caso del museo thyssen
 						TEXT = paragraphs[1]+paragraphs[2];
 						correctText = TEXT.replace("<\\" , "<");
 					}else if(url.contains("CaixaForum")){
+						// caso del caixaforum
 						TEXT = paragraphs[1]+paragraphs[2];
 						correctText = TEXT.replace("<\\" , "<");
 					}else if(url.contains("Galdiano")){
+						// caso del museo lazaro galdiano
 						TEXT = paragraphs[1]+paragraphs[2]+paragraphs[3]+paragraphs[4]+paragraphs[5];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Museo_Sorol")){
+						// caso del museo sorolla
+						TEXT = paragraphs[1]+paragraphs[2]+paragraphs[3];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Museo_Arqueol")){
+						// caso del museo arqueologico
+						TEXT = paragraphs[1]+paragraphs[2]+paragraphs[5]+paragraphs[6]+paragraphs[7]+paragraphs[8];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Museo_Nav")){
+						// caso del museo naval
+						TEXT = paragraphs[1]+paragraphs[2];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Palacio_Real")){
+						// caso del palacio real
+						TEXT = paragraphs[1]+paragraphs[2]+paragraphs[3]+paragraphs[4]+paragraphs[5]+paragraphs[6]+paragraphs[7];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Puerta_del_Sol")){
+						// caso de la pueta del sol
+						TEXT = paragraphs[1];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Plaza_Mayor")){
+						// caso de la plaza mayor
+						TEXT = paragraphs[1]+paragraphs[4]+paragraphs[5]+paragraphs[6]+paragraphs[7]+paragraphs[8];
+						String aux = TEXT.replace("El nombre de la plaza"," ");
+						correctText = aux.replace("<\\" , "<");
+					}else if(url.contains("Colegiata_de_San_Isidro")){
+						// caso de la colegiata de san isidro
+						TEXT = paragraphs[1]+paragraphs[2]+paragraphs[3];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Plaza_de_la_Villa")){
+						// caso de la plaza de la villa
+						TEXT = paragraphs[2]+paragraphs[3]+paragraphs[4];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Puerta_de_Al")){
+						// caso de la puerta de alcala
+						TEXT = paragraphs[1]+paragraphs[2]+paragraphs[3]+paragraphs[4]+paragraphs[5]+paragraphs[6];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Gran")){
+						// caso de la gran via
+						TEXT = paragraphs[1];
+						correctText = TEXT.replace("<\\" , "<");
+					}else if(url.contains("Cibeles")){
+						// caso de la plaza de cibeles
+						TEXT = paragraphs[1]+paragraphs[2]+paragraphs[3]+paragraphs[4];
 						correctText = TEXT.replace("<\\" , "<");
 					}
 					titleTextView.setText(TITLE);
